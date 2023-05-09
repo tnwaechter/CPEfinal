a3d470184f8ebdfa91130cb44546ed43007ae941
tnwaechter <133062596+tnwaechter@users.noreply.github.com>
2023-05-09 16:55:37 -0700

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,38 @@
+//Libraries
+#include <DS3231.h>
+#include <Servo.h>  
+#include <ezButton.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <dht.h>
 
+//Water Level Sensor Pins
+#define WLD_SNS_PIN A0
+
+//Temperature Humidity Sensor Pins
+#define DHT_PIN A1
+
+//DC Motor Pins
+#define ENABLE 5
+#define DIRA 6
+#define DIRB 4
+
+//Servo Motor Pin
+#define SM_PIN 22
+//Button Pin
+#define B 24
+//LCD Screen Pins
+#define RS 7
+#define E 8
+#define D4 9
+#define D5 10
+#define D6 11
+#define D7 12
+
+// LED Pins
+#define GR 13
+#define BL 4
+#define RD 5
+#define YW 25
+
+#endif 
